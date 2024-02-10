a91718e9ed5275dea6dab082d674f79d1e1cb358
mmulyana <mulyan.t20@gmail.com>
2024-02-10 11:00:00 +0700

    get ideas in homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,16 @@
 import Card from '_/components/card'
+import { db } from '_/lib/db'
 
-export default function Page() {
+export default async function Page() {
+  const ideas = await db.ideas.findMany({
+    orderBy: {
+      id: 'desc',
+    },
+  })
   return (
     <div className='pt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8'>
-      {[1, 2, 3, 4, 5, 6].map((i) => (
-        <Card key={i} />
+      {ideas.map((idea) => (
+        <Card key={idea.id} data={idea} />
       ))}
     </div>
   )
