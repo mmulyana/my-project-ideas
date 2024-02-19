efb095777c557fed695633f735d8005a5dfa89ad
mmulyana <mulyan.t20@gmail.com>
2024-02-19 10:00:00 +0700

    update: add form state in update form

--- src/app/[id]/edit/form.tsx
+++ src/app/[id]/edit/form.tsx
@@ -2,6 +2,9 @@
 
 import { useRef, useState } from 'react'
 import type { Ideas } from '@prisma/client'
+import { useFormStatus } from 'react-dom'
+import { useFormState } from 'react-dom'
+import { FormState, updateIdea } from './action'
 
 type Props = {
   data: Ideas
@@ -9,6 +12,9 @@ type Props = {
 export default function Form({ data }: Props) {
   const inputPhotoRef = useRef<HTMLInputElement>(null)
   const [imagePreview, setImagePreview] = useState<string>(data.cover)
+  const [formState, action] = useFormState<FormState>(updateIdea as any, {
+    errors: {},
+  })
 
   async function handleUpload() {
     const uploadedFile = inputPhotoRef.current?.files?.[0]
@@ -22,7 +28,7 @@ export default function Form({ data }: Props) {
 
   return (
     <div className='pt-5 pb-10'>
-      <form className='max-w-sm flex flex-col gap-4 mx-auto'>
+      <form className='max-w-sm flex flex-col gap-4 mx-auto' action={action}>
         <div className='w-full'>
           <label
             htmlFor='dropzone-file'
@@ -92,6 +98,7 @@ export default function Form({ data }: Props) {
             type='text'
             id='name'
             autoFocus
+            name="name"
             defaultValue={data.name}
             className='border text-sm rounded-lg block w-full p-2.5 bg-[#2b2b2b] border-transparent placeholder-gray-400 text-white focus:ring-4 focus:ring-blue-500/20 focus:border-blue-500'
           />
@@ -106,18 +113,52 @@ export default function Form({ data }: Props) {
           <textarea
             id='description'
             rows={4}
+            name="description"
+            defaultValue={data.description ?? ''}
             className='block p-2.5 w-full text-sm rounded-lg border border-transparent bg-[#2b2b2b] placeholder-gray-400 text-white focus:ring-4 focus:ring-blue-500/20 focus:border-blue-500'
-          >
-            {data.description}
-          </textarea>
+          ></textarea>
+          <input name='id' defaultValue={data.id} hidden />
         </div>
-        <button
-          type='submit'
-          className='py-2.5 w-full text-white rounded-lg bg-blue-600 hover:bg-blue-700 mt-1.5'
-        >
-          Update
-        </button>
+        <Submit />
       </form>
     </div>
+  )
+}
+
+function Submit() {
+  const status = useFormStatus()
+  return (
+    <button
+      disabled={status.pending}
+      type='submit'
+      className='py-2.5 w-full text-white rounded-lg bg-blue-600 hover:bg-blue-700 mt-1.5 flex gap-1 items-center justify-center'
+    >
+      {status.pending ? (
+        <>
+          <div role='status'>
+            <svg
+              aria-hidden='true'
+              className='w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+              viewBox='0 0 100 101'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                fill='currentColor'
+              />
+              <path
+                d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                fill='currentFill'
+              />
+            </svg>
+            <span className='sr-only'>Loading...</span>
+          </div>
+          Loading
+        </>
+      ) : (
+        'Update'
+      )}
+    </button>
   )
 }
