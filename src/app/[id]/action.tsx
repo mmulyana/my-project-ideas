ce0651136525ed5d6f4c75e722a2aa8028dfcd10
mmulyana <mulyan.t20@gmail.com>
2024-02-20 11:00:00 +0700

    update delete idea action

--- src/app/[id]/action.tsx
+++ src/app/[id]/action.tsx
@@ -1,3 +1,27 @@
 'use server'
 
-export async function deleteIdea(formData: FormData) {}
+import { deleteImage } from '_/lib/cloudinary'
+import { db } from '_/lib/db'
+import { extractUrl } from '_/utils/extract-url'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+export async function deleteIdea(formData: FormData) {
+  if (!formData.get('id') || !formData.get('url')) return
+
+  let url = extractUrl(formData.get('url') as string)
+  if (!url) return
+  try {
+    await deleteImage(url)
+    await db.ideas.delete({
+      where: {
+        id: parseInt(formData.get('id') as string),
+      },
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  revalidatePath('/')
+  redirect('/')
+}
